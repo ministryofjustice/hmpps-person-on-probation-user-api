f2cb580edaaf75c8dab77c4e4687879fe2da5d73
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-02-07 10:28:09 +0000

    PLT-11 Flyway db fix

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/helpers/TestBase.kt
@@ -0,0 +1,27 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers
+
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.config.PostgresContainer
+
+@ActiveProfiles("test")
+abstract class TestBase {
+
+  companion object {
+    private val pgContainer = PostgresContainer.instance
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun properties(registry: DynamicPropertyRegistry) {
+      pgContainer?.run {
+        registry.add("spring.flyway.url", pgContainer::getJdbcUrl)
+        registry.add("spring.flyway.user", pgContainer::getUsername)
+        registry.add("spring.flyway.password", pgContainer::getPassword)
+        registry.add("spring.datasource.url", pgContainer::getJdbcUrl)
+        registry.add("spring.datasource.user", pgContainer::getUsername)
+        registry.add("spring.datasource.password", pgContainer::getPassword)
+      }
+    }
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/integration/IntegrationTestBase.kt
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.jdbc.SqlMergeMode
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.helpers.TestBase
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
-abstract class IntegrationTestBase {
+@SqlMergeMode(SqlMergeMode.MergeMode.MERGE)
+abstract class IntegrationTestBase : TestBase() {
 
-  @Suppress("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   lateinit var webTestClient: WebTestClient
 }
