b2f22aa3ce9f41c0d383627bf2ee6cb76fb5c317
abdulrkalam <abdul.rahuman@digital.justice.gov.uk>
2024-02-15 13:43:53 +0000

    PLT-68 update user fix and test unit added

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/service/UserServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/service/UserServiceTest.kt
@@ -1,5 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.service
 
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -7,6 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.data.UserPatch
+import uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.data.UserPost
 import uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.jpa.entity.UserEntity
 import uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.jpa.repository.UserRepository
 import java.time.LocalDateTime
@@ -18,8 +24,7 @@ class UserServiceTest {
   @Mock
   private lateinit var userRepository: UserRepository
 
-  private val testDate = LocalDateTime.parse("2023-08-16T12:00:00")
-  private val fakeNow = LocalDateTime.parse("2023-08-17T12:00:01")
+  private val fakeNow = LocalDateTime.parse("2024-02-12T14:33:26")
 
   @BeforeEach
   fun beforeEach() {
@@ -27,9 +32,9 @@ class UserServiceTest {
   }
 
   @Test
-  fun `test getUserByCrn - returns a user details`() {
-    val userEntity1 = UserEntity(1, "abc", "123", "user1@gmail.com", false, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
-    val userEntity2 = UserEntity(2, "abc", "123456", "user2@gmail.com", false, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
+  fun `test getUserByCrn - returns list of user details`() {
+    val userEntity1 = UserEntity(1, "abc", "123", "user1@gmail.com", true, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
+    val userEntity2 = UserEntity(2, "abc", "123456", "user2@gmail.com", true, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
     val userEntityList = emptyList<UserEntity>().toMutableList()
     userEntityList.add(userEntity1)
     userEntityList.add(userEntity2)
@@ -49,5 +54,59 @@ class UserServiceTest {
     Mockito.`when`(userRepository.findAll()).thenReturn(userEntityList)
     val result = userService.getAllUsers()
     Assertions.assertEquals(userEntityList, result)
+  }
+
+  @Test
+  fun `test getUserByCrnAndId - returns a user details`() {
+    val userEntity1 = UserEntity(1, "abc", "123", "user1@gmail.com", false, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
+    val userEntity2 = UserEntity(2, "abc", "123456", "user2@gmail.com", false, LocalDateTime.parse("2024-02-12T14:33:26"), LocalDateTime.parse("2024-02-12T14:33:26"))
+    val userEntityList = emptyList<UserEntity>().toMutableList()
+    userEntityList.add(userEntity1)
+    userEntityList.add(userEntity2)
+    Mockito.`when`(userEntity1.id?.let { userRepository.findByIdAndCrn(it, userEntity1.crn) }).thenReturn(userEntity1)
+    val result = userService.getUserByCrnAndUserId("abc", 1)
+    Assertions.assertEquals(userEntity1, result)
+  }
+
+  @Test
+  fun `test user - creates and returns user`() {
+    mockkStatic(LocalDateTime::class)
+    every { LocalDateTime.now() } returns fakeNow
+
+    Mockito.`when`(userRepository.findByEmail("user1@gmail.com")).thenReturn(null)
+
+    val userEntity1 = UserEntity(id = null, crn = "abc", cprId = "123", email = "user1@gmail.com", verified = true, creationDate = fakeNow, modifiedDate = fakeNow)
+    val userPost = UserPost(
+      crn = "abc",
+      cprId = "123",
+      email = "user1@gmail.com",
+      verified = true,
+    )
+
+    Mockito.`when`(userRepository.save(any())).thenReturn(userEntity1)
+    val result = userService.createUser(userPost)
+    Mockito.verify(userRepository).save(userEntity1)
+    Assertions.assertEquals(userEntity1, result)
+    unmockkStatic(LocalDateTime::class)
+  }
+
+  @Test
+  fun `test updateUser - updates and returns user`() {
+    mockkStatic(LocalDateTime::class)
+    every { LocalDateTime.now() } returns fakeNow
+    val userEntity1 = UserEntity(id = null, crn = "abc", cprId = "123", email = "user1@gmail.com", verified = true, creationDate = fakeNow, modifiedDate = fakeNow)
+    val expectedUserEntity = UserEntity(id = null, crn = "abc", cprId = "12345", email = "user1@gmail.com", verified = true, creationDate = fakeNow, modifiedDate = fakeNow)
+    val userPatch = UserPatch(
+      crn = "abc",
+      cprId = "12345",
+      email = "user1@gmail.com",
+      verified = true,
+    )
+
+    Mockito.`when`(userRepository.save(any())).thenReturn(expectedUserEntity)
+    val result = userService.updateUser(userEntity1, userPatch)
+    Mockito.verify(userRepository).save(expectedUserEntity)
+    Assertions.assertEquals(expectedUserEntity, result)
+    unmockkStatic(LocalDateTime::class)
   }
 }
