847349ad514061666bf7f268db09f6aed54121f5
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2024-04-24 15:02:53 +0100

    PLT-222 Pipeline Security fix (#27)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.14.1"
-  kotlin("plugin.spring") version "1.9.22"
-  id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
+  val kotlinVersion = "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
+  kotlin("plugin.spring") version kotlinVersion
+  id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
   id("jacoco")
   id("org.sonarqube") version "4.0.0.2929"
-  kotlin("plugin.jpa") version "1.8.22"
-  kotlin("plugin.serialization") version "1.9.22"
+  kotlin("plugin.jpa") version kotlinVersion
+  kotlin("plugin.serialization") version kotlinVersion
 }
 
 configurations {
@@ -28,14 +29,13 @@ dependencies {
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-  implementation("org.projectlombok:lombok:1.18.26")
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
-  runtimeOnly("org.postgresql:postgresql:42.6.0")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
@@ -47,36 +47,41 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.10.0")
   implementation("commons-codec:commons-codec")
   implementation("com.google.code.gson:gson")
-  implementation("org.json:json:20231013")
+  implementation("org.json:json:20240303")
 
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   testImplementation("org.awaitility:awaitility-kotlin")
-  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-  testImplementation("org.mockito:mockito-inline:4.6.1")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+  testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.19")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.1")
-  testImplementation("org.testcontainers:localstack:1.18.1")
-  testImplementation("org.testcontainers:postgresql:1.18.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.5.3")
+  testImplementation("org.testcontainers:postgresql")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("javax.xml.bind:jaxb-api:2.3.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.34.1")
   testImplementation("com.squareup.okhttp3:okhttp:4.12.0")
-  testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
-  testImplementation("io.mockk:mockk:1.13.7")
+  testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+  testImplementation("io.mockk:mockk:1.13.10")
 
   if (project.hasProperty("docs")) {
     implementation("com.h2database:h2")
   }
 }
 
-kotlin {
-  jvmToolchain(21)
+openApi {
+  outputDir.set(project.layout.buildDirectory.dir("docs"))
+  outputFileName.set("openapi.json")
+  customBootRun.args.set(listOf("--spring.profiles.active=dev,docs"))
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
@@ -84,5 +89,21 @@ tasks {
     kotlinOptions {
       jvmTarget = "21"
     }
+  }
+}
+
+// Fix issue with springdoc-openapi-gradle-plugin https://github.com/springdoc/springdoc-openapi-gradle-plugin/issues/128
+project.tasks.named("forkedSpringBootRun").get().dependsOn(project.tasks.named("inspectClassesForKotlinIC"))
+
+tasks.test {
+  finalizedBy(tasks.jacocoTestReport)
+  // https://github.com/mockk/mockk/issues/681
+  jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
+  reports {
+    xml.required.set(true)
   }
 }
