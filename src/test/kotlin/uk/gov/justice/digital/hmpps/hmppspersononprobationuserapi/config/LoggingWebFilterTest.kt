a67dabba3f2de308e1889f54042e14a0f3769d8f
Riccardo Noviello <riccardo.noviello@digital.justice.gov.uk>
2024-05-08 16:56:11 +0100

    PLT-262: Add session id from headers to logs

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/config/LoggingWebFilterTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppspersononprobationuserapi/config/LoggingWebFilterTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppsresettlementpassportapi.config
+package uk.gov.justice.digital.hmpps.hmppspersononprobationuserapi.config
 
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
